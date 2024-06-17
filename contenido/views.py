f5d055ccad13527ea6841d56a76e2e19516a4b18
SACSZA97 <134246985+SACSZA97@users.noreply.github.com>
2024-06-17 12:13:23 -0600

    Proyecto Django nuevos stylos
    se agregaron nuevos estilos al proyecto anterior que se estaba trabajando en clases

---
+++ contenido/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render , HttpResponse
+
+# Create your views here.
+
+def principal(request):
+     return render(request,"contenido/principal.html")
+
+def contacto(request):
+
+    return render(request,"contenido/contacto.html")
+
+def formulario(request):
+
+    return render(request,"contenido/cursos.html")
